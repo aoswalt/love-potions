88e91afc84a490e4a8597088e88c176ee2d859d2
Adam Oswalt <email@adamoswalt.com>
2024-09-10 08:20:30 -0500

    Parameterize Registry
    Prep for multile registries

--- fireworks/registry.lua
+++ fireworks/registry.lua
@@ -1,26 +1,81 @@
 local Registry = {}
 
-local entity_modules = {}
+-- local modules = {}
+-- local key_field = '__entity__'
+--
+-- function Registry.key_field()
+--   return key_field
+-- end
+--
+-- function Registry.register(key, module)
+--   if modules[key] then
+--     error("entity module already mapped for " .. key)
+--   end
+--
+--   modules[key] = module
+-- end
+--
+-- ---@param ent_or_key Entity | string
+-- function Registry.lookup(ent_or_key)
+--   local key
+--
+--   if type(ent_or_key) == 'string' then
+--     key = ent_or_key
+--   elseif type(ent_or_key) == 'table' then
+--     key = ent_or_key[key_field]
+--     assert(key, 'no value found under ' .. key_field .. ' for lookup')
+--   else
+--     error('invalid type for Registry lookup, got ' .. type(ent_or_key))
+--   end
+--
+--   local mod = modules[key]
+--
+--   assert(mod, "entity module not found for " .. key)
+--
+--   return mod
+-- end
 
-function Registry.register(key, module)
-  if entity_modules[key] then
-    error("entity module already mapped for " .. key)
+function Registry.new(key_field)
+  assert(key_field, 'key_field required for Registry definition')
+
+  local new_registry = {
+    __key_field__ = key_field,
+    modules = {},
+  }
+
+  function new_registry.key_field()
+    return new_registry.key_field
   end
 
-  entity_modules[key] = module
+  function new_registry.register(key, module)
+    if new_registry.modules[key] then
+      error("entity module already mapped for " .. key)
+    end
+
+    new_registry.modules[key] = module
+  end
+
+  ---@param ent_or_key Entity | string
+  function new_registry.lookup(ent_or_key)
+    local key
+
+    if type(ent_or_key) == 'string' then
+      key = ent_or_key
+    elseif type(ent_or_key) == 'table' then
+      key = ent_or_key[new_registry.__key_field__]
+      assert(key, 'no value found under ' .. new_registry.__key_field__ .. ' for lookup')
+    else
+      error('invalid type for Registry lookup, got ' .. type(ent_or_key))
+    end
+
+    local mod = new_registry.modules[key]
+
+    assert(mod, "entity module not found for " .. key)
+
+    return mod
+  end
+
+  return new_registry
 end
 
----@param ent Entity
-function Registry.lookup(ent)
-  return Registry.lookup_by_key(ent.__entity__)
-end
-
-function Registry.lookup_by_key(key)
-  local mod = entity_modules[key]
-
-  assert(mod, "entity module not found for " .. key)
-
-  return mod
-end
-
-return Registry
+return Registry.new('__entity__')
